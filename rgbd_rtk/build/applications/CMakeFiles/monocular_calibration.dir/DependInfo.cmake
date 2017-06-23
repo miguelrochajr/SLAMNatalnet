accb71a6c3431b74f66245aa5ed5a9e76a1d7b82
Miguel Rocha Jr - Ubuntu VM <miguelsrochajr@gmail.com>
2017-06-23 11:19:57 -0700

    Arquivos de Bruno added

---
+++ rgbd_rtk/build/applications/CMakeFiles/monocular_calibration.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/msrj/Documents/rgbd_rtk/applications/monocular_calibration.cpp" "/home/msrj/Documents/rgbd_rtk/build/applications/CMakeFiles/monocular_calibration.dir/monocular_calibration.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/vtk-5.8"
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "../common"
+  "../io"
+  "../tracking"
+  "../motion_estimation"
+  "../visualization"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
